c8ee35860b8abac22648bf2fa15c8d8470b4e582
AlexanderEverett007 <xianzhe.huang@student.uq.edu.au>
2025-05-09 15:54:11 +1000

    week9(IA)

--- work_phase_3/js/reflective_design.js
+++ work_phase_3/js/reflective_design.js
@@ -6,11 +6,24 @@ document.addEventListener("DOMContentLoaded", function () {
     initAccordion(".accordion"); //Invoke the initialization function of the accordion 调用初始化手风琴功能
 });
 
+
+
+
+
+// Import the fetchGetData function from the getData.js module  
+// 从 getData.js 模块中导入 fetchGetData 函数
 import { fetchGetData } from "./modules/getData.js";
 
+// Run this block after the HTML content has fully loaded  
+// 当 HTML 内容加载完成后执行
 document.addEventListener("DOMContentLoaded", () => {
+
+// Get the container element where data will be displayed  
+// 获取用于展示成员数据的容器元素
     const container = document.getElementById("community-list");
 
+// Call the fetchGetData function to fetch member data from the API  
+// 调用 fetchGetData 函数，从 API 获取社区成员数据
     fetchGetData(
         "https://damp-castle-86239-1b70ee448fbd.herokuapp.com/decoapi/community/",
         {
